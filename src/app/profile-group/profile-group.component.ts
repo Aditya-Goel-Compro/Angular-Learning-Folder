014578f076ad565c4a8f46af67e6a01cf2c661e3
Aditya <adityagoelgithub0320@gmail.com>
2025-02-19 15:12:59 +0530

    valdation added in reactive form on /form page

--- src/app/profile-group/profile-group.component.ts
+++ src/app/profile-group/profile-group.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup  , FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup  , FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-group',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule , NgIf],
   templateUrl: './profile-group.component.html',
   styleUrl: './profile-group.component.css'
 })
@@ -13,9 +14,9 @@ export class ProfileGroupComponent {
 
   profileForm = new FormGroup
   ({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    name: new FormControl('' , [Validators.required , Validators.minLength(4)]),  
+    email: new FormControl('' , [Validators.required , Validators.email]),
+    password: new FormControl('' , [Validators.required , Validators.minLength(6) , Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}')])
   });
 
   onSubmit(){
@@ -24,5 +25,16 @@ export class ProfileGroupComponent {
 
   consoleFormData(){
     console.log(`name : ${this.profileForm.value.name} , email : ${this.profileForm.value.email} , password : ${this.profileForm.value.password}`);
+  }
+
+  get name(){
+    return this.profileForm.get('name');
+  }
+
+  get email(){
+    return this.profileForm.get('email');
+  }
+  get password(){ 
+    return this.profileForm.get('password');
   }
 }
