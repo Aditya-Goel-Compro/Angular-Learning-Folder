876c663631723b7bceee358619c61a936246f415
Aditya Goel <aditya.goel@comprotechnologies.com>
2025-02-26 13:06:22 +0530

    data from parent to child code

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Child2Component } from "./child2/child2.component";
+import { Child1Component } from './child1/child1.component';
 
 @Component({
   selector: 'app-parent',
-  imports: [RouterOutlet, RouterLink, Child2Component],
+  imports: [RouterOutlet, RouterLink, Child2Component , Child1Component],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
@@ -16,6 +17,11 @@ onUserChange(user:string){
   this.username = user ; 
 }
 
+
+
+
+
+arr= ["child1", "child2" , "child3" , "child4" , "child5"]
 }
 
 
